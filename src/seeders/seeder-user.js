7521a6f6af5864f300c60879d24992e1c3976add
NguyenMinhChien <minhchien03112003@gmail.com>
2023-07-28 23:06:33 +0700

    Tạo Tất Cả Models, Migrations và Seeders

--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -3,9 +3,16 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'admin@gmail.com',
+      password: '12345678',
+      firstName: 'Nguyen Minh',
+      lastName: 'Chien',
+      address: "THPHCM",
+      phoneNumber: "0392845906",
+      gender: 1,
+      image: "",
+      roleId: "R1",
+      positionId: "Developer",
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
