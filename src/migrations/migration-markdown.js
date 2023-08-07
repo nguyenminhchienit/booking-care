aaad233919f9a3442c8c88b4c34f33bcc5d6298d
NguyenMinhChien <minhchien03112003@gmail.com>
2023-08-07 15:34:51 +0700

    create model and migration markdown

---
+++ src/migrations/migration-markdown.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Markdowns', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+    //   doctorId
+    //   clinicId
+    //   specialtyid
+    //   contentHTML
+    //   contentMarkdown
+    //   description
+      doctorId: {
+        type: Sequelize.INTEGER
+      },
+      clinicId: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.TEXT('long')
+      },
+      specialtyId: {
+        type: Sequelize.INTEGER
+      },
+      contentHTML: {
+        type: Sequelize.TEXT('long')
+      },
+      contentMarkdown: {
+        type: Sequelize.TEXT('long')
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Markdowns');
+  }
+};
